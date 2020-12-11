580904b0fdc0592753e3e03a05119a9e5f7e6606
ilgiz <ilgiz@smarthead.ru>
2020-12-11 16:12:22 +0300

    Implementation of LoginPage check

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -11,4 +11,29 @@ def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()    
+    page.should_be_login_link() 
+
+def test_forms_that_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+    
+# def test_on_main_page_should_be_login_url(browser):
+    # link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    # page = LoginPage(browser, link)
+    # page.open()
+    # page.should_be_login_url()
+    
+# def test_on_login_page_should_be_login_form(browser):
+    # link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    # page = LoginPage(browser, link)
+    # page.open()
+    # page.should_be_login_form()
+
+
+# def test_on_login_page_should_be_register_form(browser):
+    # link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    # page = LoginPage(browser, link)
+    # page.open()
+    # page.should_be_register_form()
